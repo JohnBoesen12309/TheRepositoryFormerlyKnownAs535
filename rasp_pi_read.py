a5bd00aa2666ce586d46b2a3c73503912ace6b77
John Boesen <82904895+JohnBoesen12309@users.noreply.github.com>
2023-12-07 09:22:29 -0500

    Update rasp_pi_read.py

--- rasp_pi_read.py
+++ rasp_pi_read.py
@@ -1,56 +1,107 @@
 import serial
 import time
+import matplotlib.pyplot as plt
 
 class TimeSync:
     def __init__(self) -> None:
-        self.ser1=serial.Serial('dev/ttyACM0',9600)
-        self.ser2=serial.Serial('dev/ttyACM1',9600)
+        self.ser1=serial.Serial('COM5',115200)
+        self.ser2=serial.Serial('COM9',115200)
     
     def ReadData(self,port:int) -> list:
         if port == 1:
-            indata:str = self.ser1.readline()
+            indata:str = self.ser1.readline().decode('utf-8')
         elif port == 2:
-            indata:str = self.ser2.readline()
+            indata:str = self.ser2.readline().decode('utf-8')
         else:
             raise ValueError('No Data Read:Port must be 1 or 2')
-        j=0
-        tmp=''
-        data=[0,0]
-        for i in indata:
-            if j == 2:
-                break
-            elif i == ' ':
-                data[j]=float(tmp)
-                j+=1
-            elif i == '\0':
-                break
-            else:
-                tmp+=i
-        print(data)
-        return data
+        if indata == 'waiting for sync message':
+            print('Waiting for Sync')
+            return -1
+        tmp=['','']
+        tmp = indata.split('#')
+        data = tmp[1]
+        #print('Recieved Time:',data,'Recieved Data:',tmp[0]+' C')
+        return float(data)
 
-    def NetworkDelay(self,data_arr:list,port:int) -> float:
-        delay = time.clock_gettime() - data_arr[0]
-        print(delay)
+    def ClockDrift(self,data_arr,tm) -> float:
+        delay = abs(data_arr - tm)
+        print('Clock Drift:',delay)
         return delay
     
-    def ClockDrift(time1:float,time2:float) -> None:
-        if time1 < 0:
-            raise ValueError('Time from Port 1 device is negative')
-        if time2 < 0:
-            raise ValueError('Time from port 2 device is negative')
-        print(time1-time2)
-
+    def NetworkDelay(self,port) -> None:
+        tm = time.time()
+        send = 'T'+str(tm)
+        if port == 1:
+            self.ser1.write(send.encode('utf-8'))
+            while self.ser1.out_waiting > 0:
+                pass
+            delay = len(bytes(send,'utf-8'))/(time.time()-tm)
+        elif port == 2:
+            self.ser2.write(send.encode('utf-8'))
+            while self.ser2.out_waiting > 0:
+                pass
+            delay = len(bytes(send,'utf-8'))/(time.time()-tm)
+        print("Network Delay:",delay)
+        return delay
+        
+    def TimeSync(self,port,tm):
+        if port == 1:
+            time_out = tm
+            time_out = 'T'+str(time_out)
+            self.ser1.write(time_out.encode("utf-8"))
+        elif port == 2:
+            time_out = tm
+            time_out = 'T'+str(time_out)
+            self.ser2.write(time_out.encode("utf-8"))
+        else:
+            raise ValueError('No Data Sent:Port must be 1 or 2')
 
 if __name__ == '__main__':
-    while(1):
-        prog = TimeSync()
-        data_arr1 = prog.ReadData(1)
-        data_arr2 = prog.ReadData(2)
-        delay1 = prog.NetworkDelay(data_arr1)
-        delay2 = prog.NetworkDelay(data_arr2)
-        prog.ClockDrift(delay1,delay2)
+    prog = TimeSync()
+    clckplt1 = []
+    clckplt2 = []
+    i = 1
+    x = []
+    while(i <=100):
+        x.append(i)
+        tm = time.time()
+        prog.TimeSync(1,tm)
+        data1 = prog.ReadData(1)
+        drift1 = prog.ClockDrift(data1,tm)
+        tm = time.time()
+        prog.TimeSync(2,tm)
+        data2 = prog.ReadData(2)
+        drift2 = prog.ClockDrift(data2,tm)
+        if drift1 < 100:
+            clckplt1.append(drift1)
+        else:
+            clckplt1.append(0)
+        if drift2 < 100:
+            clckplt2.append(drift2)
+        else:
+            clckplt2.append(0)
+        #prog.ClockDrift(data2,data2)
+        print(i)
+        i+=1
+    avg1 = sum(clckplt1)/len(clckplt1)
+    avg2 = sum(clckplt2)/len(clckplt2)
+    print('Average Clock Drift (Device 1):',avg1)
+    print('Average Clock Drift (Device 2):',avg2)
 
+    
+    plt.plot(x,clckplt1, label ='Arduino 1')
+    plt.plot(x,clckplt2,label = 'Arduino 2')
+    ax = plt.gca()
+    ax.set_xlim([5,len(x)])
+    ax.set_ylim([-1.75,1.75])
+    plt.xlabel('Iterations')
+    plt.ylabel('Clock Drift')
+    plt.title('Clock Drift through Wired Connections')
+    plt.legend()
+    plt.show()
 
     
     
+'''
+print(prog.NetworkDelay(1),prog.NetworkDelay(2))
+'''
